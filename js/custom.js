c3042a3abf2a8a9bbc09142aa004c070d7c08a0c
atra700 <atra700@gmail.com>
2025-01-22 15:14:28 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,44 @@
+$(function () {
+
+    const mvs = new Swiper('.main_visual_slide', {
+        loop: true,
+        speed: 2000,
+        effect: "fade",
+        autoplay: {
+            delay:2500,
+            disableOnInteraction: false,
+        },     
+        navigation: {
+            nextEl: '.main_visual .arrows .next',
+            prevEl: '.main_visual .arrows .prev',
+          },
+    })
+});
+
+// 모바일 메뉴
+
+$(function () {
+    $('.header .mbtn').on('click', function () {
+        $(this).toggleClass('on');
+        $('.header .gnb').toggleClass('on');
+    });
+
+    $('.header .gnb>ul>li>a').on('click', function (e) {
+        if ($('.header .gnb').hasClass('on')) {
+            e.preventDefault();
+            $('.header .gnb>ul ul').stop().slideUp();
+            $(this).next().stop().slideDown();
+        }
+
+    });
+
+    $(window).on('resize', function () {
+        $('.header .mbtn').removeClass('on');
+        $('.header .gnb').removeClass('on');
+        $('.header .gnb>ul ul').removeAttr('style')
+    });
+
+    $('.header .gnb').on('wheel touchmove', function (e) {
+        e.preventDefault();
+    })
+})
